c3174d779c2a418c63ba33b3e5ee22e4574be098
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-17 15:57:45 -0400

    Add DB connector

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const dotenv = require("dotenv")
 dotenv.config()
  
 const connectDb = async () => {
-    try {
-        const client = new Client({
-            user: process.env.PGUSER,
-            host: process.env.PGHOST,
-            database: process.env.PGDATABASE,
-            password: process.env.PGPASSWORD,
-            port: process.env.PGPORT
-        })
- 
-        await client.connect()
-        const res = await client.query('SELECT * FROM some_table')
-        console.log(res)
-        await client.end()
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const client = new Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT
+    })
+
+  await client.connect()
+  const res = await client.query('SELECT * FROM some_table')
+  console.log(res)
+  await client.end()
+  } catch (error) {
+  console.log(error)
+  }
 }
- 
+
 connectDb()
